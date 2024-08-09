6ef381bf33d56d5305d03da361afa65d2f52aa29
safi.saeed1999 <safi.saeed1999@gmail.com>
2024-08-09 18:18:58 +0330

    fix comamnds

--- Russel_CLI/Program.cs
+++ Russel_CLI/Program.cs
@@ -64,30 +64,32 @@ public class ApiClient
         }
     }
 
-    
-
     public async Task<string> Get(string cluster, string key)
     {
-        var url = $"/api/get/{cluster}/{key}";
-        var request = new RestRequest(url, Method.Get);
-
-        var response = await _client.ExecuteAsync(request);
-
-        if (response.IsSuccessful)
+        try
         {
-            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<byte[]>>(response.Content);
-            if (apiResponse.IsSuccess)
+            var url = $"/api/get/{cluster}/{key}";
+            var request = new RestRequest(url, Method.Get);
+
+            var response = await _client.ExecuteAsync(request);
+
+            if (response.IsSuccessful)
             {
-                return Encoding.UTF8.GetString(apiResponse.Data).DecodeBase64ToString();
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse<byte[]>>(response.Content);
+                if (apiResponse.IsSuccess)
+                {
+                    return Encoding.UTF8.GetString(apiResponse.Data).DecodeBase64ToString();
+                }
+                else
+                {
+                    throw new Exception("Failed to get value");
+                }
             }
-            else
-            {
-                throw new Exception("Failed to get value");
-            }
+            return response.ErrorMessage;
         }
-        else
+        catch (Exception ex)
         {
-            throw new Exception($"Error getting value: {response.ErrorMessage}");
+            throw new Exception($"Error getting value: {ex.Message}");
         }
     }
 
@@ -108,6 +110,40 @@ public class ApiClient
         }
     }
 
+    public async Task<List<string>> GetKeysOfCluster(string clusterName)
+    {
+        var url = $"/api/get_keys/{clusterName}";
+        var request = new RestRequest(url, Method.Get);
+
+        var response = await _client.ExecuteAsync(request);
+
+        if (response.IsSuccessful)
+        {
+            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<List<string>>>(response.Content);
+            return apiResponse.Data;
+        }
+        else
+        {
+            throw new Exception($"Error getting keys of  cluster: {response.ErrorMessage}");
+        }
+    }
+
+    public async Task SetCluster(string cluster)
+    {
+        var url = $"/api/set_cluster/{cluster}";
+        var request = new RestRequest(url, Method.Post);
+
+        var response = await _client.ExecuteAsync(request);
+
+        if (response.IsSuccessful)
+        {
+            Console.WriteLine($"cluster [{cluster}] set");
+        }
+        else
+        {
+            Console.WriteLine($"Error set cluster: {response.ErrorMessage}");
+        }
+    }
     public async Task ClearCluster(string cluster)
     {
         var url = $"/api/clear_cluster/{cluster}";
@@ -151,14 +187,30 @@ public class ApiClient
 
         if (response.IsSuccessful)
         {
-            var apiResponse = JsonConvert.DeserializeObject<ApiResponse<List<string>>>(response.Content);
-            return apiResponse.Data;
+            try
+            {
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse<List<string>>>(response.Content);
+                return apiResponse.Data;
+            }
+            catch
+            {
+                var apiResponse = JsonConvert.DeserializeObject<ApiResponse<string>>(response.Content);
+                if (!string.IsNullOrEmpty(apiResponse.Data))
+                {
+                    return new List<string>() { apiResponse.Data };
+                }
+                else
+                {
+                    return new List<string>() { "no cluster found" };
+                }
+            }
         }
         else
         {
             throw new Exception($"Error getting clusters: {response.ErrorMessage}");
         }
     }
+
 
     public async Task<ushort> GetPort()
     {
@@ -183,7 +235,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var client = new ApiClient("http://192.168.1.8:5022");
+        var client = new ApiClient("http://127.0.0.1:6060");
         await CommandHandler.HandleCommand(client);
     }
 }
@@ -211,7 +263,6 @@ public static class CommandHandler
                         var cluster = parts[1];
                         var key = parts[2];
                         var value = parts[3];
-                        //var value = Encoding.UTF8.GetBytes(parts[3]);
                         await client.Set(cluster, key, value);
                         break;
                     }
@@ -223,14 +274,13 @@ public static class CommandHandler
                 case "set_cluster" when parts.Length == 2:
                     {
                         var cluster = parts[1];
-                        await client.Set(cluster, "", string.Empty);  // assuming set cluster is just an empty set
-                        Console.WriteLine($"Cluster [{cluster}] set");
+                        await client.SetCluster(cluster); 
                         break;
                     }
-                case "get_keys" when parts.Length == 2:
+                case "keys*" when parts.Length == 2:
                     {
                         var cluster = parts[1];
-                        var keys = await client.GetAllClusters();  // Assuming there's a get keys method in API
+                        var keys = await client.GetKeysOfCluster(cluster);
                         Console.WriteLine($"Keys in cluster [{cluster}]: {string.Join(", ", keys)}");
                         break;
                     }
@@ -257,30 +307,10 @@ public static class CommandHandler
                         Console.WriteLine($"Cleared cluster [{cluster}]");
                         break;
                     }
-                case "clear_all":
-                    {
-                        await client.ClearAll();
-                        Console.WriteLine("Cleared all clusters");
-                        break;
-                    }
-                case "get_clusters":
+                case "cluster*":
                     {
                         var clusters = await client.GetAllClusters();
-                        var port = await client.GetPort();
-                        Console.WriteLine($"Clusters on port {port} are: {string.Join(", ", clusters)}");
-                        break;
-                    }
-                case "connection_string":
-                    {
-                        var ip = GetLocalIPAddress();
-                        var port = await client.GetPort();
-                        Console.WriteLine($"{ip}:{port}");
-                        break;
-                    }
-                case "port":
-                    {
-                        var port = await client.GetPort();
-                        Console.WriteLine($"Port is: {port}");
+                        Console.WriteLine($"Clusters are: {string.Join(", ", clusters)}");
                         break;
                     }
                 case "help":
@@ -301,13 +331,6 @@ public static class CommandHandler
     {
         Console.Write("> ");
     }
-
-    private static string GetLocalIPAddress()
-    {
-        // Assuming this method gets the local IP address of the machine
-        return "127.0.0.1";
-    }
-
     private static void PrintHelp()
     {
         Console.WriteLine("Commands:");
@@ -316,10 +339,7 @@ public static class CommandHandler
         Console.WriteLine("get [cluster name] [key] - Get value for key in cluster");
         Console.WriteLine("delete [cluster name] [key] - Delete key from cluster");
         Console.WriteLine("clear_cluster [cluster name] - Clear all keys in cluster");
-        Console.WriteLine("clear_all - Clear all clusters");
-        Console.WriteLine("get_clusters - Get list of all clusters");
-        Console.WriteLine("connection_string - Get server connection string");
-        Console.WriteLine("port - Get server port");
+        Console.WriteLine("cluster* - Get list of all clusters");
         Console.WriteLine("-v - Show version");
         Console.WriteLine("help - Show this help message");
     }
