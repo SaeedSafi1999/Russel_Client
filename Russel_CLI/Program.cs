2c83d99f1f1cd7b170d24b96e5032fa26ec69af8
safi.saeed1999 <safi.saeed1999@gmail.com>
2024-08-09 19:33:17 +0330

    fix

--- Russel_CLI/Program.cs
+++ Russel_CLI/Program.cs
@@ -7,7 +7,7 @@ public class Program
 {
     public static async Task Main(string[] args)
     {
-        var configJson = await File.ReadAllTextAsync("Config.json");
+        var configJson = await File.ReadAllTextAsync("CLI_Config.json");
         var config = JsonSerializer.Deserialize<Config>(configJson);
         var port = config.ApiSettings.Port;
         var client = new ApiClient($"http://127.0.0.1:{port}");
